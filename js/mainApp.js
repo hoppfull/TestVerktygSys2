32cc15e120a0a54c4ea7aaa56bb371cfa776cbc5
Björn Kihlberg <hoppfull@gmail.com>
2016-03-03 15:13:31 +0100

    Städat upp och nollställt kodbas

---
+++ js/mainApp.js
@@ -0,0 +1,9 @@
+(function (pages) { // Author: Bjorn
+    var mainApp = angular.module('mainApp' ['ngRoute']);
+    
+    mainApp.config(function ($routeProvider) {
+        
+    });
+    
+    
+})(['login', 'teacher', 'student', 'admin']);
