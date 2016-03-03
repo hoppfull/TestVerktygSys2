3b0a4a3cf6ebe77c7b28e3851aaa7172a91a7572
Björn Kihlberg <hoppfull@gmail.com>
2016-03-03 15:20:36 +0100

    Implementerat route-funktionalitet
    Behöver bara implementera controllers vid laddning av undersidor samt
    att inkludera javascript i index.html.

--- js/mainApp.js
+++ js/mainApp.js
@@ -1,9 +1,15 @@
-(function (pages) { // Author: Bjorn
+var mainApp = (function (pages) { // Author: Bjorn
     var mainApp = angular.module('mainApp' ['ngRoute']);
     
     mainApp.config(function ($routeProvider) {
-        
+        pages.forEach(function (page) {
+            $routeProvider.when('/' + page, {
+                templateUrl: 'html/' + page + '.html',
+                controller: page + 'Controller'
+            });
+        });
+        $routeProvider.otherwise({redirectTo: '/login'});
     });
     
-    
+    return mainApp;
 })(['login', 'teacher', 'student', 'admin']);
