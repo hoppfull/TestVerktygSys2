98103ff9bae0278c7fdc63970d4dffecfb5fcbfb
Minh Tran <quocminh2303@gmail.com>
2016-03-04 19:52:33 +0100

    fix admincontroller
    teacher ->admin

--- js/admincontroller.js
+++ js/admincontroller.js
@@ -1,6 +1,6 @@
 (function() {
     angular.module("indexApp").controller("adminController", function($scope, $http) {
-        $http.get('../data/teacher.json').success(function(response) {
+        $http.get('../data/admins.json').success(function(response) {
             $scope.admins = response.admins;
         });   
     });
