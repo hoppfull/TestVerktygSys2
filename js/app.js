f3aa9cca7057f43769f254d29583acaeeac011eb
Björn Kihlberg <hoppfull@gmail.com>
2016-03-08 12:59:13 +0100

    Implemented route for webpage

--- js/app.js
+++ js/app.js
@@ -5,10 +5,23 @@
     });
 
     mainApp.config(function ($routeProvider) {
-        $routeProvider.when('/login', {
-            templateUrl: 'html/login.html',
-            controller: 'loginController'
-        })
+        $routeProvider
+            .when('/login', {
+                templateUrl: 'html/login.html',
+                controller: 'loginController'
+            })
+            .when('/admin', {
+                templateUrl: 'html/admin.html',
+                controller: 'adminController'
+            })
+            .when('/student', {
+                templateUrl: 'html/student.html',
+                controller: 'studentController'
+            })
+            .when('/teacher', {
+                templateUrl: 'html/student.html',
+                controller: 'teacherController'
+            })
             .otherwise({ redirectTo: '/login' });
     });
 } ());
