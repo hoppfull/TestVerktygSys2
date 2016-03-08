9c9f0c1c87174aef8c2b1711ebb1f29ccb2338b5
Eric Björkvall <scorlion_@hotmail.com>
2016-03-08 15:53:54 +0100

    student site update

--- js/teachercontroller.js
+++ js/teachercontroller.js
@@ -2,8 +2,6 @@
     angular.module("indexApp").controller("teacherController", function($scope, $http) {
         $http.get('../data/teacher.json').success(function(response) {
             $scope.teachers = response.teachers;
-
-        });
-            
+        });   
     });
 }());
