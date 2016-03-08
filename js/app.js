63547afcdfec10b89ba68b4c2ba3648c0ed958a9
Björn Kihlberg <hoppfull@gmail.com>
2016-03-08 14:48:38 +0100

    Implemented logic for loading data into dataService

--- js/app.js
+++ js/app.js
@@ -1,17 +1,51 @@
 (function () {
     var indexApp = angular.module("indexApp", ['ngRoute']);
 
-    indexApp.controller('mainController', function ($scope) {
+    indexApp.controller('mainController', function ($scope, $http, dataService) {
+        (function () { // Author: bjorn
+            // This function loads data into dataService
+            $http.get('../data/admins.json').success(function (response) {
+                response.admins.forEach(admin => {
+                    admin.type = "admin";
+                    dataService.addUser(admin);
+                });
+            });
+            $http.get('../data/students.json').success(function (response) {
+                response.students.forEach(student => {
+                    student.type = "student";
+                    dataService.addUser(student);
+                });
+            });
+            $http.get('../data/teachers.json').success(function (response) {
+                response.teachers.forEach(teacher => {
+                    teacher.type = "teacher";
+                    dataService.addUser(teacher);
+                });
+            });
+        })();
     });
-    
-    indexApp.service('loginService', function() {
+
+    indexApp.service('dataService', function () {
+        var users = [];
+
+        var addUser = function (user) {
+            users.push(user);
+        };
+
+        return {
+            users: users,
+            addUser: addUser
+        };
+    });
+
+    indexApp.service('loginService', function () {
         var user = null;
-        
-        var logout = function() {
+
+        var logout = function () {
             user = null;
             location.href = '';
         };
-        
+
         return {
             user: user,
             logout: logout
