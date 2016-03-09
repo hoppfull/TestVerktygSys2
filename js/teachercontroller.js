9e14a4853cc795aa23acf2b6314a51c92907496d
Minh Tran <quocminh2303@gmail.com>
2016-03-09 14:15:55 +0100

    delete myFun in teachercontroller.js

--- js/teachercontroller.js
+++ js/teachercontroller.js
@@ -1,18 +1,14 @@
 (function () {
-    angular.module("indexApp").controller("teacherController", function ($scope, dataService, loginService) {
+    angular.module("indexApp").controller("teacherController", function ($scope, dataService, loginService) {        
         $scope.loggedIn = false;
         if (loginService.user != null) {
             $scope.loggedIn = true;
             $scope.TeacherName = loginService.user.username;
             $scope.quizzes = dataService.quizzes.filter(quiz => quiz.author === loginService.user.username);
-
+            
             $scope.logout = function () {
                 loginService.logout();
             };
         }
-
-        $scope.myFunc = function () {
-            console.log("tjena");
-        };
     });
 } ());
