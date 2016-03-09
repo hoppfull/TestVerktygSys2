9e543e680d0e498dc961f41b4188f06f7e392be6
Björn Kihlberg <hoppfull@gmail.com>
2016-03-09 15:40:07 +0100

    added data awareness to dataprovider

--- js/teachercontroller.js
+++ js/teachercontroller.js
@@ -1,36 +1,36 @@
-(function () {
-    angular.module("indexApp").controller("teacherController", function ($scope, dataService, loginService) {
+(function() {
+    angular.module("indexApp").controller("teacherController", function($scope, dataService, loginService) {
         $scope.loggedIn = false;
         if (loginService.user != null) {
             $scope.loggedIn = true;
             $scope.TeacherName = loginService.user.username;
             updateQuizList();
-            
-            
-            $scope.logout = function () {
+
+            $scope.myFunc = function() {
+                console.log(dataService.removeQuiz('hej', 'Minh'));
+                updateQuizList();
+            };
+
+            $scope.logout = function() {
                 loginService.logout();
             };
 
-            $scope.newQuizSubmit = function () {
-                if (!$scope.quizzes.find(quiz => quiz.name.toLowerCase() === $scope.newQuizName.toLowerCase())) {
-                    var newQuiz = {
-                        author: $scope.TeacherName,
-                        time: $scope.newQuizTime,
-                        name: $scope.newQuizName,
-                        subject: $scope.newQuizSubject,
-                        questions: []
-                    };
-                    dataService.addQuiz(newQuiz);
-                    updateQuizList();
-                    
-                    $scope.addQuizMsg = '';
-                } else {
-                    $scope.addQuizMsg = 'Namn finns redan!';
-                }
+            $scope.newQuizSubmit = function() {
+                var newQuiz = {
+                    author: $scope.TeacherName,
+                    time: $scope.newQuizTime,
+                    name: $scope.newQuizName,
+                    subject: $scope.newQuizSubject,
+                    questions: []
+                };
+                $scope.addQuizMsg =
+                    dataService.addQuiz(newQuiz)
+                        ? ''
+                        : 'Namn finns redan!';
+                updateQuizList();
             };
 
             function updateQuizList() {
-                
                 $scope.quizzes = dataService.quizzes.filter(quiz => quiz.author === loginService.user.username);
             }
         }
