bd46cfefa1977324c4732dd13beb7ea62b4d11d2
Minh Tran <quocminh2303@gmail.com>
2016-03-09 10:58:52 +0100

    implented show students in teacher.html

--- js/teachercontroller.js
+++ js/teachercontroller.js
@@ -1,10 +1,20 @@
 (function() {
     angular.module("indexApp").controller("teacherController", function($scope, dataService, $location) {
         var students = dataService.users.filter(user => user.type === "student");
-        $scope.students = students;
-        $scope.exams = students.exam;
         
-        
+        var x = [];
+        for (var i = 0; i < students.length; i++) {
+            for (var j = 0; j < students[i].exam.length; j++) {
+                x.push({
+                    username: students[i].username,
+                    examName: students[i].exam[j].name,
+                    availableFrom: students[i].exam[j].availableFrom,
+                    availableUntil: students[i].exam[j].availableUntil
+                });
+            }
+        }
+        console.log(x);
+        $scope.p = x;
         
     });
 }());
