b48318e73bf90168ddf9101f60dc95d8111395f1
Björn Kihlberg <hoppfull@gmail.com>
2016-03-09 14:10:31 +0100

    update textboxex in teacher.html

--- js/teachercontroller.js
+++ js/teachercontroller.js
@@ -1,30 +1,18 @@
 (function () {
     angular.module("indexApp").controller("teacherController", function ($scope, dataService, loginService) {
-        /*var students = dataService.users.filter(user => user.type === "student");
-
-        var x = [];
-        students.forEach(student => student.exam.forEach(exam => x.push({
-            username: student.username,
-            examName: exam.name,
-            availableFrom: exam.availableFrom,
-            availableUntil: exam.availableUntil
-        })));
-
-        $scope.p = x; */
         $scope.loggedIn = false;
         if (loginService.user != null) {
             $scope.loggedIn = true;
             $scope.TeacherName = loginService.user.username;
             $scope.quizzes = dataService.quizzes.filter(quiz => quiz.author === loginService.user.username);
-            
+
             $scope.logout = function () {
                 loginService.logout();
             };
         }
 
-
-        var deletebutton = function (x) {
-
-        }
+        $scope.myFunc = function () {
+            console.log("tjena");
+        };
     });
 } ());
