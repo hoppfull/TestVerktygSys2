6c071417b13ea2f076b2b364fbfd261a014ed0d8
Eric Björkvall <scorlion_@hotmail.com>
2016-03-09 14:00:26 +0100

    merge with master

--- js/admincontroller.js
+++ js/admincontroller.js
@@ -1,7 +1,17 @@
 (function() {
-    angular.module("indexApp").controller("adminController", function($scope, $http) {
+    angular.module("indexApp").controller("adminController", function($scope, $http, dataService, $window) {
+        /* Example: using dataService: */
+        // get all teachers:
         $http.get('../data/admins.json').success(function(response) {
             $scope.admins = response.admins;
+           
         });   
+        $http.get('../data/users.json').success(function(response) {
+            $scope.users = response.users;
+        })
+        
+        $scope.sendTest = function() {
+        $window.alert("Proven är skickade");
+      };
     });
 }());
--- js/loginController.js
+++ js/loginController.js
@@ -1,5 +1,18 @@
-(function () {
-    angular.module("indexApp").controller('loginController', function ($scope) {
+(function() {
+    angular.module("indexApp").controller("loginController", function($scope, $http, loginService, dataService) {
 
+        $scope.login = function() {
+            if (!$scope.username || !$scope.password) {
+                $scope.error = 'Fyll i alla fält!';
+            } else {
+                var user = dataService.users.find(x => x.username === $scope.username && x.password === $scope.password);
+                if (!user) {
+                    $scope.error = "Användaren finns inte eller felaktigt lösenord";
+                } else {
+                    loginService.user = user;
+                    location.href = '#/' + user.type;
+                }
+            }
+        };
     });
-})();
+} ());
