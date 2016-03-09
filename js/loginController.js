99547b476b1fb8c20733383bfc91f70849d51214
Gustaf Stark <gustafstark@gmail.com>
2016-03-09 11:07:04 +0100

    Fixed loginController

--- js/loginController.js
+++ js/loginController.js
@@ -1,19 +1,18 @@
-(function () {
-    angular.module("indexApp").controller("loginController", function ($scope, $http, loginService) {
-        $http.get('../data/users_TEMP.json').success(function (response) {
-            $scope.login = function () {
-                if(!$scope.username || !$scope.password) {
-                    $scope.error = 'Fyll i alla fält!';
+(function() {
+    angular.module("indexApp").controller("loginController", function($scope, $http, loginService, dataService) {
+
+        $scope.login = function() {
+            if (!$scope.username || !$scope.password) {
+                $scope.error = 'Fyll i alla fält!';
+            } else {
+                var user = dataService.users.find(x => x.username === $scope.username && x.password === $scope.password);
+                if (!user) {
+                    $scope.error = "Användaren finns inte eller felaktigt lösenord";
                 } else {
-                    var user = response.users.find(x => x.username === $scope.username && x.password === $scope.password);
-                    if (!user) {
-                        $scope.error = "Användaren finns inte eller felaktigt lösenord";
-                    } else {
-                        loginService.user = user;
-                        location.href = '#/' + user.type;
-                    }
+                    loginService.user = user;
+                    location.href = '#/' + user.type;
                 }
-            };
-        });
+            }
+        };
     });
 } ());
