ccaa4f999e813c8bcd55d314048efaee89aa0ac0
Örjan Carlsson <orjancarlsson90@gmail.com>
2016-03-09 09:55:35 +0100

    Örjan login

--- js/loginController.js
+++ js/loginController.js
@@ -1,5 +1,18 @@
-(function () {
-    angular.module("indexApp").controller('loginController', function ($scope) {
+(function() {
+    angular.module("indexApp").controller("loginController", function($scope, $http) {
+        $http.get('../data/users_TEMP.json').success(function(response) {
+            var users = response.users;
+
+            $scope.login = function() {
+                var uname = $scope.username;
+                var pword = $scope.password;
+                if (uname == null || pword == null) {
+                    $scope.error = "Fyll i saker";
+                }
+
+            };
+        });
+
 
     });
-})();
+} ());
