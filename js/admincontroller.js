1a50e7825b1deb0dec2c3a638a8138c0851555fd
Björn Kihlberg <hoppfull@gmail.com>
2016-03-09 09:47:07 +0100

    example for using dataService in adminController.js

--- js/admincontroller.js
+++ js/admincontroller.js
@@ -1,5 +1,12 @@
 (function() {
-    angular.module("indexApp").controller("adminController", function($scope, $http) {
+    angular.module("indexApp").controller("adminController", function($scope, $http, dataService) {
+        /* Example: using dataService: */
+        // get all teachers:
+        var teachers = dataService.users.filter(user => user.type === "teacher");
+        var SnowWhite = dataService.users.find(user => user.username === "Snow White");
+        console.log(teachers);
+        console.log(SnowWhite);
+        
         $http.get('../data/admins.json').success(function(response) {
             $scope.admins = response.admins;
         });   
