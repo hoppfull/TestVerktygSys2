7a4e20789990985a48664aa14782e59f6d509d2f
Björn Kihlberg <hoppfull@gmail.com>
2016-03-14 16:20:33 +0100

    tjena

--- js/admincontroller.js
+++ js/admincontroller.js
@@ -5,8 +5,9 @@
         $scope.logout = function () {
             loginService.logout();
         };
+        
         function updateLists() {
-            $scope.subjects = dataService.getSubjects();
+            $scope.exams = dataService.getExams().filter(exam => exam.sentToAdmin && !exam.sentToStudent);
             $scope.students = dataService.getUsers().filter(user => user.type === 'student');
             $scope.admins = dataService.getUsers().filter(user => user.type === 'admin');
             $scope.teachers = dataService.getUsers()
@@ -39,9 +40,7 @@
                 firstName: $scope.newUserFirstName,
                 lastName: $scope.newUserLastName,
                 password: $scope.newUserPassword,
-                type: $scope.showStudents ? "student" : $scope.showTeachers ? "teacher" : $scope.showAdmins ? "admin" : "",
-                studentClass: $scope.showStudents ? $scope.newUserStudentClass : "",
-                subjects: []
+                type: $scope.showStudents ? "student" : $scope.showTeachers ? "teacher" : $scope.showAdmins ? "admin" : ""
             };
 
             if (dataService.addUser(newUser)) {
@@ -56,54 +55,7 @@
                 updateLists();
             }
         };
-
-        /*
-        $scope.sendTest = function() {
-            $window.alert("Proven är skickade");
-        };
-
-        $scope.removeRowExams = function(testIndex) {
-            $scope.tests.splice(testIndex, 1);
-        };
-
-        $scope.addUser = function() {
-            $scope.users.push({
-                occupation: $scope._occupation,
-                firstName: $scope._firstName,
-                lastName: $scope._lastName,
-                username: $scope._userName,
-                password: $scope._password
-            });
-            $scope._occupation = "";
-            $scope._firstName = "";
-            $scope._lastName = "";
-            $scope._userName = "";
-            $scope._password = "";
-        };
-        $scope.userToRemove;
-        $scope.removeRowUser = function(hash) {
-            $scope.userToRemove = hash;
-        };
-        $scope.deleteUser = function() {
-            if ($scope.userToRemove != null) {
-                $scope.users.splice($scope.userToRemove, 1);
-                $scope.userToRemove = null;
-            }
-
-        }
-        $scope.questionType = function(type) {
-            return type === "SingleChoice" ? "radio" : "checkbox";
-        };*/
-        /*  setTimeout(function(){
-              loginService.logout();
-          }, 3000);*/
-        /*
-      $scope.AdminLogout = function() {
-          loginService.logout();
-      }*/
-        /*
-        $scope.hej = function() {
-            $(".datepicker").datepicker();
-        };*/
+        
+        
     });
 } ());
